9cc5d9596fa7344254fc61ec80ec691c465664f3
eamonboyle <blaowskate@hotmail.com>
2020-03-11 11:50:28 +0000

    Add a mutation to add messages

--- server/schema/resolvers.ts
+++ server/schema/resolvers.ts
@@ -25,6 +25,31 @@ const resolvers = {
             return chats.find(c => c.id === chatId);
         },
     },
+
+    Mutation: {
+        addMessage(root: any, { chatId, content}: any) {
+            const chatIndex = chats.findIndex(c => c.id === chatId);
+
+            if (chatIndex === 1) return null;
+
+            const chat = chats[chatIndex];
+
+            const messagesIds = messages.map(currentMessage => Number(currentMessage.id));
+            const messageId = String(Math.max(...messagesIds) + 1);
+            const message = {
+                id: messageId,
+                createdAt: new Date(),
+                content,
+            };
+            
+            messages.push(message);
+            chat.messages.push(messageId);
+            chats.splice(chatIndex, 1);
+            chats.unshift(chat);
+
+            return message;
+        },
+    },
 };
 
 export default resolvers;
