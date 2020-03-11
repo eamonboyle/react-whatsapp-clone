53c81db52d8ca2bcffa4a7eae17afc8acee2c11f
eamonboyle <blaowskate@hotmail.com>
2020-03-11 12:24:18 +0000

    Rewrote the cache to update server

--- client/src/graphql/queries/chats.query.ts
+++ client/src/graphql/queries/chats.query.ts
@@ -1,16 +1,11 @@
 import gql from 'graphql-tag';
+import * as fragments from '../fragments';
 
 export default gql`
-    query Chats {
-        chats {
-            id
-            name
-            picture
-            lastMessage {
-                id
-                content
-                createdAt
-            }
-        }
+  query Chats {
+    chats {
+      ...Chat
     }
+  }
+  ${fragments.chat}
 `;
